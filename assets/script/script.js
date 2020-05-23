18c9310c10b1e072f8d229552845cb858f115de7
Dario <dario.apaz85@gmail.com>
2020-05-22 22:03:36 -0400

    first commit proyecto-final

---
+++ assets/script/script.js
@@ -0,0 +1,25 @@
+$(function(){
+  
+    $("a").click(function(event){
+       if (this.hash  !=="") {
+        event.preventDefault();
+ 
+      var gato = this.hash; 
+ 
+       $("html,body").animate( {
+         scrollTop: $(gato).offset().top
+       }, 800, function() {  
+           window.location.hash = gato;
+ 
+       });
+      }
+ 
+ });
+ 
+ $('[data-toggle="popover"]').popover();
+ 
+ });
+
+ $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+  });
